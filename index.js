b7f17fa7188b137fb36103116110156433c9ce7e
William Walsh <wmdwalsh0@gmail.com>
2021-06-28 22:13:15 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2; 
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 10;
+let num4 = 6;
+let mod = num3 % num4;
+
+let max = Math.max(1, 2, 3, 4, 20);
